563fa6b2f06b8cc770c4c391bbac1accefab08a6
Bartłomiej Sobczuk <bartlomiej.sobczuk@gmail.com>
2015-08-22 18:22:11 +0200

    menu

--- app/scripts/states/MenuState.es6.js
+++ app/scripts/states/MenuState.es6.js
@@ -1,9 +1,23 @@
+import GameState from './states/GameState';
+
 export default {
     preload() {
-        this.load.image('bear1', 'images/bear1.png');
+        this.load.image('background', 'images/starfield.jpg');
+        this.load.image('button', 'images/play.png');
     },
 
     create() {
-        this.add.sprite(0, 0, 'bear1');
+        this.add.tileSprite(0, 0, 800, 600, 'background');
+        this.add.button(this.world.centerX - 60, this.world.centerY - 50, 'button', this.playGame);
+        let style = {
+            font: "32px Arial",
+            fill: "#ff0044",
+        };
+        this.add.text(this.world.centerX -65, this.world.centerY -100, 'Play GEJM', style);
+    },
+
+    playGame() {
+        this.state.add('GameState', GameState);
+        this.state.start('GameState');
     }
 }
