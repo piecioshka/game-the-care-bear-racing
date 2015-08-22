e048399fd2571733cecd8aeafadd304ba00f13a4
Bartłomiej Sobczuk <bartlomiej.sobczuk@gmail.com>
2015-08-22 18:52:43 +0200

    working menu

--- app/scripts/index.es6.js
+++ app/scripts/index.es6.js
@@ -1,10 +1,12 @@
 import Menu from './states/MenuState';
+import GameState from './states/GameState';
 
 class Game extends Phaser.Game {
 
 	constructor() {
 		super(800, 600, Phaser.AUTO, 'content', null);
 		this.state.add('MenuState', Menu);
+        this.state.add('GameState', GameState);
 		this.state.start('MenuState');
 	}
 
